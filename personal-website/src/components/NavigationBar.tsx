cb63fe74dd5b20918b10cc9635114b7072cddc4f
jwiggen <jwiggen@siue.edu>
2021-08-07 18:51:31 -0500

    try again again

--- personal-website/src/components/NavigationBar.tsx
+++ personal-website/src/components/NavigationBar.tsx
@@ -3,9 +3,9 @@ import { Form, Navbar } from 'react-bulma-components'
 function NavigationBar(): JSX.Element {
 	return (
 		<Form.Field horizontal={true} className='navbar'>
-			<Navbar.Item href='#'>Home</Navbar.Item>
-			<Navbar.Item href='#music'>Music</Navbar.Item>
-			<Navbar.Item href='#about'>About</Navbar.Item>
+			<Navbar.Item href='/'>Home</Navbar.Item>
+			<Navbar.Item href='music'>Music</Navbar.Item>
+			<Navbar.Item href='about'>About</Navbar.Item>
 		</Form.Field>
 	)
 }
