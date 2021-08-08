511f4c0c1de0890f54a19f880c0418c0bd36e05f
jwiggen <jwiggen@siue.edu>
2021-08-07 19:44:22 -0500

    try againagainagain

--- personal-website/src/components/NavigationBar.tsx
+++ personal-website/src/components/NavigationBar.tsx
@@ -4,8 +4,8 @@ function NavigationBar(): JSX.Element {
 	return (
 		<Form.Field horizontal={true} className='navbar'>
 			<Navbar.Item href='/'>Home</Navbar.Item>
-			<Navbar.Item href='music'>Music</Navbar.Item>
-			<Navbar.Item href='about'>About</Navbar.Item>
+			<Navbar.Item href='/music'>Music</Navbar.Item>
+			<Navbar.Item href='/about'>About</Navbar.Item>
 		</Form.Field>
 	)
 }
