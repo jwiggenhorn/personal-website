8d8d23f0637c3db4da3abfd5a39cf54a7e468197
jwiggen <jwiggen@siue.edu>
2021-08-08 11:00:17 -0500

    deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from 'react-dom'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './components/App'
 
 ReactDOM.render(
-	<HashRouter>
+	<BrowserRouter basename='/personal-website'>
 		<App />
-	</HashRouter>,
+	</BrowserRouter>,
 	document.getElementById('root')
 )
